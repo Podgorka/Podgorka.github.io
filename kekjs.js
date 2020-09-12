45262bf2cb9c5ad513f70582ad8f48b85387068f
Andrew Podgorny <andr02tagan@gmail.com>
2020-09-12 19:25:44 +0300

    Add files via upload

--- kekjs.js
+++ kekjs.js
@@ -1,5 +1,19 @@
-var car = {
-	speed: 228, weels: 4, name: "kek"
-};
-var x = car.speed;
-document.write(x);
+function car (speed, weels, name){
+	this.speed=speed;
+	this.weels=weels;
+	this.name=name;
+}
+let w = 22114;
+let car1 = new car(228, 4, "koka");
+document.write(car1.weels + " колеса у машины " + car1.name);
+let massiv = new Array(14, 88, 2, 28);
+let mda = massiv[1];
+let mdaString = mda+"";
+document.write("<p></p>" + massiv[0] + "~" + mda + " \f \f \f" + mdaString.length + ". а вот зто тангенс числа " +
+w + " : " + Math.tan(w));
+var m1 = ["a", "b", "c"];
+var m2 = ["e", "f", "g","h"];
+var mm = m1.concat(m2);
+mm.forEach(function(item, i, mm) {
+  document.write(" " + item + " ");
+});
